42de712b9939276f74bbe5240873e8484048c42f
Yujin031009 <115140447+Yujin031009@users.noreply.github.com>
2024-08-06 15:45:41 +0900

    ++

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { WebView } from "react-native-webview";
+import { WEB_CLIENT_URL } from "./constants/url";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const HomeScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView source={{ uri: WEB_CLIENT_URL }} style={styles.webview} />
     </View>
   );
-}
+};
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }} // 내비게이션 헤더 숨기기
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    paddingTop: Platform.OS === "ios" ? 0 : 25,
+  },
+  webview: {
+    flex: 1,
   },
 });
+
+export default App;
---
+++ constants/URL.js
@@ -0,0 +1,1 @@
+export const WEB_CLIENT_URL = "https://togathering-react.netlify.app/";
